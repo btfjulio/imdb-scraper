7e129e6df1ea45928d50ada4757b17d8ca144f0a
Bruno Tostes <btfjulio@hotmail.com>
2020-10-11 10:09:15 -0300

    add test to write yaml funciton

--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -49,3 +49,20 @@ describe "#scrape_movie" do
      expect(dark_night).to eq(expected)
   end
 end
+
+
+describe "#write_yaml" do
+  it "expect to save YAML file with movie" do
+    movie  = {
+      cast: [ "Christian Bale", "Heath Ledger", "Aaron Eckhart" ],
+      director: "Christopher Nolan",
+      storyline: "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
+      title: "The Dark Knight",
+      year: 2008
+    }
+    write_yaml(movie)
+    movie_file = YAML::load_file('movies.yml')
+
+    expect(movie_file).to eq(movie)
+  end
+end
